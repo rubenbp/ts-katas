1441d20604ca98723af71fc2aec815dd3188c99b
Rubén Bernárdez <ruben.bernardez@biko2.com>
2020-06-13 21:26:31 +0200

    add comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   env: {
-    node: true,
-    es6: true,
-    jest: true,
+    node: true, // disable error from node global functions
+    jest: true, // disable error from jest global functions
   },
   plugins: ['jest'],
-  parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'prettier/@typescript-eslint'],
+  parser: '@typescript-eslint/parser', // parse typescript
+  extends: [
+    'eslint:recommended', // eslint recommended rules
+    'prettier/@typescript-eslint', // disable formatting rules of prettier
+  ],
 }
