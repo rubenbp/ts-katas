882555bc8c78bde949b240187bc682f17bf4ab8d
Rubén Bernárdez <ruben.bernardez@biko2.com>
2020-06-13 21:53:32 +0200

    add eslint parser options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,18 @@
 module.exports = {
   env: {
-    node: true, // disable error from node global functions
-    jest: true, // disable error from jest global functions
+    es6: true, // enable es6 global variables
+    node: true, // enable node global variables
+    jest: true, // enable jest global variables
   },
   parser: '@typescript-eslint/parser', // parse typescript
   extends: [
     'eslint:recommended', // eslint recommended rules
   ],
+  parserOptions: {
+    ecmaVersion: 2018, // to support syntax version
+    sourceType: 'module', // to use ECMAscript modules
+    ecmaFeatures: {
+      jsx: true, // allow to eslint to analize jsx and tsx files
+    },
+  },
 }
