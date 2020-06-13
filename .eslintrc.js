1c049ef42d7ad20e8155f69c95a895f229b5c55f
Rubén Bernárdez <ruben.bernardez@biko2.com>
2020-06-13 21:44:07 +0200

    remove prettier config to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,8 @@ module.exports = {
     node: true, // disable error from node global functions
     jest: true, // disable error from jest global functions
   },
-  plugins: ['jest'],
   parser: '@typescript-eslint/parser', // parse typescript
   extends: [
     'eslint:recommended', // eslint recommended rules
-    'prettier/@typescript-eslint', // disable formatting rules of prettier
   ],
 }
